76ac1eb4e04245a7bd452aa4ba2e0eeff2424c39
scotchg <scotchg@sample.com>
2018-03-21 15:24:23 +0900

    Remove unused import.

--- auth-provider/src/main/java/com/github/scotchg/sample/auth/provider/controller/web/LoginController.java
+++ auth-provider/src/main/java/com/github/scotchg/sample/auth/provider/controller/web/LoginController.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
